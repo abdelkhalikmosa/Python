532dbf1045af89626600d503a1f8c44b1fc5625d
Abdelkhalik Mosa <mosaa@westminster.ac.uk>
2019-04-17 15:01:10 +0100

    basic configurations

--- loggingTutorial.py
+++ loggingTutorial.py
@@ -8,4 +8,12 @@ There are 5 levels indicating the severity of evenets
 4. error
 5. critical
 '''
-logging.debug('Debugging ...')
+# Change logging level and send the logs to a specific file rather than the console.
+logging.basicConfig(level=logging.INFO, filename="micado.log", filemode='w', format='%(asctime)s - %(process)d - %(levelname)s - %(message)s')
+
+# Various logging levels
+logging.debug(' Debugging message ... ')
+logging.info(' Info message ...')
+logging.warning(' Warning message ... ')
+logging.error(' Error message ...')
+logging.critical(' Critical message ... ')
