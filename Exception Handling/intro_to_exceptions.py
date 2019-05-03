35b7f8ef36855b87df2e56a83b29bf6031e508b1
Abdelkhalik Mosa <mosaa@westminster.ac.uk>
2019-05-03 12:45:48 +0100

    Updating the README

--- Exception Handling/intro_to_exceptions.py
+++ Exception Handling/intro_to_exceptions.py
@@ -16,16 +16,33 @@ except:
 # The program terminates once it encounters an error (syntax or exception) and any remaining code is not going to be
 # executed as long as the error is not corrected (syntax) or handeled (exception) 
 
-print("after exception")
-
-# Raising an exception
-# Use raise to throw an exception if a condition occurs
-
-first_name = 'Yasser'
-if len(first_name) < 8:
-    raise Exception('The length of the firstname should be greater than 7 letters')
-
-print("after exception")
+print(sys.platform)
 
 # The AssertionError Exception
-assert('Linux' in sys.platform), "This code runs only in Linux"
+try:
+    assert('Linux' in sys.platform), "This code runs only in Linux"
+except AssertionError as e:
+    print(e)
+
+
+try:
+    with open('template.yaml') as template:
+        read_data = template.read()
+except FileNotFoundError as e:
+    print(e)
+
+
+"""
+validates the password set by the user
+"""
+def set_password(user_password):
+    # Raising an exception
+    # Use raise to throw an exception if a condition occurs
+    if len(user_password) < 10:
+        raise Exception('The password should have at least 10 alphanumeric characters')
+    print('password is successfully set')
+
+try:
+    set_password("123")
+except:
+    print('The password is less than 10 alphanumeric characters')
--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-Python related tutorials tha covers different aspects of the Python language
+# Python tutorial
+#### A number of various Python tutorials that covers different aspects of the Python language
 
-1. Using OS module to interact with the operating system. 
-2. Using logging module
-3. *args and **kwargs
-4. Object Oriented Programming in Python 
+## Topics covered
+###1. Using OS module to interact with the operating system. 
+###2. Using logging module
+###3. *args and **kwargs
+###4. Object Oriented Programming in Python 
